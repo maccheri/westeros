e5884862f2a32d5f3cc8577be59d5b8b0e52484d
Felipe Maccheri <felipemaccheri@gmail.com>
2018-09-25 14:25:36 -0300

    Adicionando Link nos cards de cada casa e estilo no hover dos cards

--- src/card/Card.jsx
+++ src/card/Card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { 
 	Container,
   Row, 
@@ -8,19 +9,21 @@ import './card.css';
 
 const Card = (props) => (
 	<Container className="house-card">
-		<Row>
-			<Col xs="8" className="house-info">
-				<h2>Nome da Casa</h2>
-				<p>Coat of Arms</p>
-				<p>Região</p>
-			</Col>
-			<Col xs="4" className="lord-info">
-			<div className="info">
-				<p><b>Nome do Lorde Principal</b></p>
-				<p>Ano de Nascimento</p>
-			</div>
-			</Col>
-		</Row>
+		<Link to="/detail/123" style={{ textDecoration: 'none', color: 'unset' }}>
+			<Row>
+				<Col xs="8" className="house-info">
+					<h2>Nome da Casa</h2>
+					<p>Coat of Arms</p>
+					<p>Região</p>
+				</Col>
+				<Col xs="4" className="lord-info">
+				<div className="info">
+					<p><b>Nome do Lorde Principal</b></p>
+					<p>Ano de Nascimento</p>
+				</div>
+				</Col>
+			</Row>
+		</Link>
 	</Container>
 );
 
